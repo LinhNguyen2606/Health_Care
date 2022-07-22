c07b9add2f28e340d20843fa35060291c7259693
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2022-07-22 15:39:26 +0700

    Done register and login function

---
+++ client/src/services/userService.js
@@ -0,0 +1,16 @@
+import axios from '../axios';
+
+const handleLoginApi = (email, password) => {
+    return axios.post('api/login', { email, password });
+};
+
+const handleRegisterApi = (fullname, email, password) => {
+    return axios.post('api/register', { fullname, email, password });
+};
+
+const activationForEmail = (activation_token) => {
+    console.log(activation_token);
+    return axios.post('/api/activation', activation_token);
+};
+
+export { handleLoginApi, handleRegisterApi, activationForEmail };

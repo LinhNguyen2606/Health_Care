6bc0bb2fa0405c1d30e16a86e27f2ff6ca1e1cf3
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2022-08-23 22:09:19 +0700

    Design Interface Function Create Medical Appointment Appointment

--- client/src/store/actions/adminActions.js
+++ client/src/store/actions/adminActions.js
@@ -268,3 +268,27 @@ export const saveDetailDoctor = (data) => {
         }
     };
 };
+
+export const fetchAllScheduleTime = () => {
+    return async (dispatch, getState) => {
+        try {
+            const res = await getAllCodeService('TIME');
+            if (res && res.errCode === 0) {
+                dispatch({
+                    //action
+                    type: actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_SUCCESS,
+                    dataTime: res.data,
+                });
+            } else {
+                dispatch({
+                    type: actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILED,
+                });
+            }
+        } catch (e) {
+            console.log('FETCH_ALLCODE_SCHEDULE_TIME_FAILED: ', e);
+            dispatch({
+                type: actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILED,
+            });
+        }
+    };
+};

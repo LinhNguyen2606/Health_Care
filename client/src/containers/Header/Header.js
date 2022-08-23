c5263b58f7f538dca97afdf67f6381ad67993762
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2022-08-23 20:41:30 +0700

    Create User Authorization Menu When Login

--- client/src/containers/Header/Header.js
+++ client/src/containers/Header/Header.js
@@ -2,16 +2,47 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions';
 import Navigator from '../../components/Navigator';
-import { adminMenu } from './menuApp';
+import { adminMenu, doctorMenu } from './menuApp';
 import './Header.scss';
-import { LANGUAGES } from '../../utils';
+import { LANGUAGES, USER_ROLE } from '../../utils';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
+import _ from 'lodash';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuApp: [],
+        };
+    }
+
     handleChangeLanguage = (language) => {
         this.props.changeLanguageForApp(language);
     };
+
+    componentDidMount() {
+        let { userInfo } = this.props;
+        let menu = [];
+        if (userInfo && !_.isEmpty(userInfo)) {
+            let role = userInfo.roleId;
+            if (role === USER_ROLE.ADMIN) {
+                menu = adminMenu;
+            }
+
+            if (role === USER_ROLE.DOCTOR) {
+                menu = doctorMenu;
+            }
+
+            if (role === USER_ROLE.PATIENT) {
+                menu = doctorMenu;
+            }
+        }
+
+        this.setState({
+            menuApp: menu,
+        });
+    }
 
     render() {
         const { processLogout, language, userInfo, user } = this.props;
@@ -22,42 +53,78 @@ class Header extends Component {
                         {/* nút logout */}
                         <Link to="/home">
                             <div className="btn btn-logout" onClick={processLogout} title="Log out">
-                                <i className="fas fa-sign-out-alt" style={{ marginLeft: '104rem ' }}></i>
+                                <i className="fas fa-sign-out-alt" style={{ marginLeft: '106rem ' }}></i>
                             </div>
                         </Link>
                     </>
                 ) : (
                     <>
-                        {/* thanh navigator */}
-                        <div className="header-tabs-container">
-                            <Navigator menus={adminMenu} />
-                        </div>
+                        {user?.userInfo?.roleId === 'R2' ? (
+                            <>
+                                {/* thanh navigator */}
+                                <div className="header-tabs-container">
+                                    <Navigator menus={this.state.menuApp} />
+                                </div>
+                                <div className="languages" style={{ marginLeft: '25rem' }}>
+                                    <span className="welcome">
+                                        <FormattedMessage id="homeheader.welcome" />
+                                        {userInfo && userInfo.fullname ? userInfo.fullname : ''} !
+                                    </span>
+                                    <span
+                                        className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
+                                        onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
+                                    >
+                                        VN
+                                    </span>
+                                    <span
+                                        className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}
+                                        onClick={() => this.handleChangeLanguage(LANGUAGES.EN)}
+                                    >
+                                        EN
+                                    </span>
+                                </div>
 
-                        <div className="languages">
-                            <span className="welcome">
-                                <FormattedMessage id="homeheader.welcome" />
-                                {userInfo && userInfo.fullname ? userInfo.fullname : ''} !
-                            </span>
-                            <span
-                                className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
-                                onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
-                            >
-                                VN
-                            </span>
-                            <span
-                                className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}
-                                onClick={() => this.handleChangeLanguage(LANGUAGES.EN)}
-                            >
-                                EN
-                            </span>
-                        </div>
+                                {/* nút logout */}
+                                <Link to="/login">
+                                    <div className="btn btn-logout" onClick={processLogout} title="Log out">
+                                        <i className="fas fa-sign-out-alt"></i>
+                                    </div>
+                                </Link>
+                            </>
+                        ) : (
+                            <>
+                                {/* thanh navigator */}
+                                <div className="header-tabs-container">
+                                    <Navigator menus={this.state.menuApp} />
+                                </div>
 
-                        {/* nút logout */}
-                        <Link to="/login">
-                            <div className="btn btn-logout" onClick={processLogout} title="Log out">
-                                <i className="fas fa-sign-out-alt"></i>
-                            </div>
-                        </Link>
+                                <div className="languages">
+                                    <span className="welcome">
+                                        <FormattedMessage id="homeheader.welcome" />
+                                        {userInfo && userInfo.fullname ? userInfo.fullname : ''} !
+                                    </span>
+                                    <span
+                                        className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
+                                        onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
+                                    >
+                                        VN
+                                    </span>
+                                    <span
+                                        className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}
+                                        onClick={() => this.handleChangeLanguage(LANGUAGES.EN)}
+                                    >
+                                        EN
+                                    </span>
+                                </div>
+
+                                {/* nút logout */}
+                                <Link to="/login">
+                                    <div className="btn btn-logout" onClick={processLogout} title="Log out">
+                                        <i className="fas fa-sign-out-alt"></i>
+                                    </div>
+                                </Link>
+                            </>
+                        )}
                     </>
                 )}
             </div>

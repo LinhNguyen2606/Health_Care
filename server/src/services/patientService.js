74bbb04f29ff0852bfc9a40dc7d5e981c46dda4a
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2022-09-02 10:45:53 +0700

    Configure Email Send Appointments With NodeMailer

--- server/src/services/patientService.js
+++ server/src/services/patientService.js
@@ -1,5 +1,6 @@
 import db from '../models/index';
 require('dotenv').config();
+import emailService from './emailService';
 
 let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
@@ -10,6 +11,14 @@ let postBookAppointment = (data) => {
                     errMessage: 'Missing required parameters!',
                 });
             } else {
+                await emailService.sendSimpleEmail({
+                    receiverEmail: data.email,
+                    patientName: 'Patient name',
+                    time: '8:00 - 9:00 Chủ nhật 2-9-2022',
+                    doctorName: 'Doctor name',
+                    redirectLink: 'http://localhost:3000/home',
+                });
+
                 //upsert patient
                 let user = await db.User.findOrCreate({
                     where: { email: data.email },
